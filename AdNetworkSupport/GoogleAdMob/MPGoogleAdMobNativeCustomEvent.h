9787fe3da3a4e74053046f3211cdc9915b951100
Amad Ayub <hayub@google.com>
2017-03-02 15:12:34 -0800

    Adds Google native adapter.

---
+++ AdNetworkSupport/GoogleAdMob/MPGoogleAdMobNativeCustomEvent.h
@@ -0,0 +1,14 @@
+#if __has_include(<MoPub / MoPub.h>)
+#import <MoPub/MoPub.h>
+#else
+#import "MPNativeCustomEvent.h"
+#endif
+
+@import GoogleMobileAds;
+
+@interface MPGoogleAdMobNativeCustomEvent : MPNativeCustomEvent
+
+/// Sets the preferred location of the AdChoices icon.
++ (void)setAdChoicesPosition:(GADAdChoicesPosition)position;
+
+@end
